04a81f72e7e62ae4074020c2e125296405295f36
aysimasavas <aysimasavas1@gmail.com>
2023-01-25 18:33:12 +0300

    resilience4j integrated

--- order-service/src/main/java/com/aysimasavas/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/aysimasavas/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.aysimasavas.orderservice.controller;
 
 import com.aysimasavas.orderservice.dto.OrderRequest;
 import com.aysimasavas.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,13 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
         orderService.placeOrder(orderRequest);
         return "order place succesfully";
+    }
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "something went wrong, please order after some time!!!!!";
     }
 }
